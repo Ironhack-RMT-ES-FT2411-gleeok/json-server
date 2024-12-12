139b1f7661cd987fa7ca442084ff0ee7313ba34e
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2024-12-12 14:39:02 +0100

    done

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const jsonServer = require("json-server")
+
+const server = jsonServer.create() // creamos el servidor
+
+const middlewares = jsonServer.defaults() // configuraciones bases de un servidor
+
+server.use(middlewares)
+
+const allowAccessFromAnywhere = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  next()
+} // configuracion para permir acceso de cualquier cliente en el mundo a mi servidor
+
+server.use(allowAccessFromAnywhere)
+
+const routes = jsonServer.router("db.json") // genera todas las rutas de acceso posibles
+
+server.use(routes)
+
+const PORT = process.env.PORT
+
+server.listen(PORT, () => {
+  console.log(`JSON server ejecutandose en el puerto ${PORT}`)
+  console.log(`Acceso local en http://localhost:${PORT}`)
+}) // activar el servidor y ponerlo a escuchar llamadas
